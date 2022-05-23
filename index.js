6b2dd1b60aa9fd25e33421ff6d845f5743fae595
saron <467294171@qq.com>
2022-05-23 19:13:28 +0800

    updata 20220523-2

--- index.js
+++ index.js
@@ -39,9 +39,9 @@ router.post("/api/creatcode", async (ctx) => {
     const token = fs.readFileSync('/.tencentcloudbase/wx/cloudbase_access_token', 'utf-8');//获取容器推送的token
     let data={
       cloudbase_access_token:token,
-      page: 'pages/v2/index/index',
-      scene: 'roletype='+roleType+'&schoolid='+schoolId,//最大32个可见字符，只支持数字，大小写英文以及部分特殊字符：!#$&'()*+,/:;=?@-._~，其它字符请自行编码为合法字符（因不支持%，中文无法使用 urlencode 处理，请使用其他编码方式）
-      // scene: 'roleType='+roleType+'&schoolId='+schoolId,
+      page: 'pages/v2/index/index?',
+      // scene: 'roletype='+roleType+'&schoolid='+schoolId,//最大32个可见字符，只支持数字，大小写英文以及部分特殊字符：!#$&'()*+,/:;=?@-._~，其它字符请自行编码为合法字符（因不支持%，中文无法使用 urlencode 处理，请使用其他编码方式）
+      scene: urlencode('roleType='+roleType+'&schoolId='+schoolId),
       checkPath: true,
       envVersion: 'release'
       }
@@ -71,7 +71,8 @@ let creatcode= async(wxinfo,msgData)=>{
       body: JSON.stringify(msgData||{
         cloudbase_access_token:token,
         page: 'pages/v2/index/index',
-        scene: 'roletype='+roleType+'&schoolid='+schoolId,//最大32个可见字符，只支持数字，大小写英文以及部分特殊字符：!#$&'()*+,/:;=?@-._~，其它字符请自行编码为合法字符（因不支持%，中文无法使用 urlencode 处理，请使用其他编码方式）
+        // scene: 'roletype='+roleType+'&schoolid='+schoolId,//最大32个可见字符，只支持数字，大小写英文以及部分特殊字符：!#$&'()*+,/:;=?@-._~，其它字符请自行编码为合法字符（因不支持%，中文无法使用 urlencode 处理，请使用其他编码方式）
+        scene: urlencode('roleType='+roleType+'&schoolId='+schoolId),
         // scene: 'roleType='+roleType+'&schoolId='+schoolId,
         checkPath: true,
         envVersion: 'release'
